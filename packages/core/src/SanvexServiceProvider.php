86da4a879a25e586b23ae020036dc4d305af5a2d
Narayan Dhakal <narayandhakal443@gmail.com>
2026-04-17 08:55:57 +0545

    feat(core): add base package scaffold

---
+++ packages/core/src/SanvexServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sanvex\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class SanvexServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
